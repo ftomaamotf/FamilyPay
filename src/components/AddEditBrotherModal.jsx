ba393826b774c887c7396df0019ed7a4d5532aca
ftomaamotf <ftomaamotf@gmail.com>
2026-09-12 11:47:30 +0300

    Update AddEditBrotherModal.jsx

--- src/components/AddEditBrotherModal.jsx
+++ src/components/AddEditBrotherModal.jsx
@@ -15,7 +15,14 @@ import {
 } from 'lucide-react';
 
 export const AddEditBrotherModal = ({ isOpen, onClose, brotherToEdit = null }) => {
-  const { addBrother, updateBrother, deleteBrother, brothers, activeAdminId } = useFinance();
+  const {
+  addBrother,
+  updateBrother,
+  deleteBrother,
+  brothers,
+  activeAdminId,
+  currentUser
+} = useFinance();
 
   const [name, setName] = useState('');
   const [accountNumber, setAccountNumber] = useState('');
